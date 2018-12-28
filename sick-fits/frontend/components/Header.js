206eb0ccc556140dad71ac144f5ac6ee927dbc90
christine-aqui <aqui.christine@gmail.com>
2018-12-28 12:15:52 -0500

    adding styled components to customer page

--- sick-fits/frontend/components/Header.js
+++ sick-fits/frontend/components/Header.js
@@ -1,10 +1,31 @@
 import Nav from "./Nav";
+import Link from "next/link";
+import styled, { ThemeProvider, injectGlobal } from "styled-components";
+
+const Logo = styled.h1`
+  font-size: 4rem;
+  margin-left: 2rem;
+  position: relative;
+  z-index: 2;
+  transform: skew(-7deg);
+  a {
+    padding: 0.5 rem 1 rem;
+    background: ${props => props.theme.red};
+    color: white;
+    text-decoration: none;
+    text-transform: uppercase;
+  }
+`;
 
 const Header = () => {
   return (
     <div>
       <div className="bar">
-        <a href="">Sick Fits</a>
+        <Logo>
+          <Link href="/">
+            <a>Sick Fits</a>
+          </Link>
+        </Logo>
         <Nav />
       </div>
       <div className="sub-bar">
